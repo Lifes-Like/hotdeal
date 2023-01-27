5fc578a36d08ac48b4cd4135548f3dabf37a9c65
hj <mytomouryes@gmail.com>
2023-01-28 01:47:52 +0900

    hotdeal title + deal_info added

--- scraping.py
+++ scraping.py
@@ -1,6 +1,23 @@
+import requests
+import re
 from bs4 import BeautifulSoup
-from time import sleep
-from datetime import datetime
-from os import path, makedir
 
-def 
+headers = {
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+        }
+
+source = 'https://www.fmkorea.com'
+url = f'{source}/index.php?mid=hotdeal&sort_index=pop&order_type=desc&page=1'
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, 'lxml')
+deals = soup.find_all('li', attrs={'class':re.compile('hotdeal0$')})
+
+for deal in deals:
+    title = deal.find('h3', attrs={'class':'title'}).get_text()
+    deal_info = deal.find_all('a', attrs={'class':'strong'})
+    # link = deal.find_all('a', attrs={'class':'strong'})[0]['href']
+    # price = deal.find_all('a', attrs={'class':'strong'})[1].get_text()
+    # delivery = deal.find_all('a', attrs={'class':'strong'})[2].get_text()
+    print(f'''{name}, 가격 :{deal_info[1].get_text()}, 배송 : {deal_info[2].get_text()}
+            {source}{deal_info[0]['href']}''')
